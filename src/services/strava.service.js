26045604b48f3ba44a3c00d72cc09c8df8848ad2
Nicolas Skolaris <nicolas@defide.co.jp>
2021-05-15 18:31:17 +0900

    removed client secret

--- src/services/strava.service.js
+++ src/services/strava.service.js
@@ -1,9 +1,7 @@
 import axios from 'axios'
 
-const clientId = '66062'
-const clientSecret = 'a2c33ad6c389d42f128730b6694dc1f123695c35'
-
-// http://www.strava.com/oauth/authorize?client_id=66062&response_type=code&redirect_uri=http://localhost:8080&approval_prompt=force&scope=read_all,activity:read_all
+const clientId = process.env.VUE_APP_STRAVA_CLIENT_ID
+const clientSecret = process.env.VUE_APP_STRAVA_CLIENT_SECRET
 
 export function getAuthUrl () {
   return 'https://www.strava.com/oauth/authorize?client_id=66062&redirect_uri=http://localhost:8080&response_type=code&scope=read_all,activity:read_all'
