7dbcf93dc66280fc8984c1bda9ee8c2646d5dbb9
Nicolas Skolaris <nicolas@defide.co.jp>
2021-05-29 19:16:12 +0900

    feat: eslint

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
 export function createSegment (data) {
-  return axios.post(`http://localhost:3000/segment`, data)
+	return axios.post(`http://localhost:3000/segment`, data)
 }
 
 export function getSegments () {
-  return axios.get(`http://localhost:3000/segments`)
+	return axios.get(`http://localhost:3000/segments`)
 }
 
 export function getSegmentsByProximity (range, geom, lat, lng) {
-  if (geom) return axios.get(`http://localhost:3000/segment_search?range=${range}&geom=${geom}`)
-  else return axios.get(`http://localhost:3000/segment_search?range=${range}&lat=${lat}&lng=${lng}`)
+	if (geom) return axios.get(`http://localhost:3000/segment_search?range=${range}&geom=${geom}`)
+	return axios.get(`http://localhost:3000/segment_search?range=${range}&lat=${lat}&lng=${lng}`)
 }
